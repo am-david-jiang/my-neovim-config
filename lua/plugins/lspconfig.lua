f53774cddab80aea50a9ae3fbb9c683537a6713e
am-david-jiang <jiangqiang2009@126.com>
2024-10-25 13:47:54 +0800

    chore: lspconfig setting:q

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -21,6 +21,7 @@ local servers = {
 	"cssls",
 	"html",
 	"ts_ls",
+	"eslint",
 	"pyright",
 	"bashls",
 	"jsonls",
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,50 +1,49 @@
 local M = {
-  "williamboman/mason.nvim",
-  cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
-  dependencies = {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  }
+	"williamboman/mason.nvim",
+	dependencies = {
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
 }
 
 function M.config()
-  local tools = {
-    "prettierd", -- prettierd formatter
-    "stylua", -- lua formatter
-    "isort", -- python formatter
-    "black", -- python formatter
-    "pylint", -- python linter
-    "eslint_d", -- js linter
-  }
+	local tools = {
+		"prettierd", -- prettierd formatter
+		"stylua", -- lua formatter
+		"isort", -- python formatter
+		"black", -- python formatter
+		"pylint", -- python linter
+		"eslint_d", -- js linter
+	}
 
-  require("mason").setup {
-    ui = {
-      border = "rounded",
-      icons = {
-        package_pending = " ",
-        package_installed = "󰄳 ",
-        package_uninstalled = " 󰚌",
-      },
+	require("mason").setup({
+		ui = {
+			border = "rounded",
+			icons = {
+				package_pending = " ",
+				package_installed = "󰄳 ",
+				package_uninstalled = " 󰚌",
+			},
 
-      keymaps = {
-        toggle_server_expand = "<CR>",
-        install_server = "i",
-        update_server = "u",
-        check_server_version = "c",
-        update_all_servers = "U",
-        check_outdated_servers = "C",
-        uninstall_server = "X",
-        cancel_installation = "<C-c>",
-      },
-    },
+			keymaps = {
+				toggle_server_expand = "<CR>",
+				install_server = "i",
+				update_server = "u",
+				check_server_version = "c",
+				update_all_servers = "U",
+				check_outdated_servers = "C",
+				uninstall_server = "X",
+				cancel_installation = "<C-c>",
+			},
+		},
 
-    max_concurrent_installers = 10,
-  }
+		max_concurrent_installers = 10,
+	})
 
-  local mason_tool_installer = require("mason-tool-installer")
+	local mason_tool_installer = require("mason-tool-installer")
 
-  mason_tool_installer.setup({
-    ensure_installed = tools,
-  })
+	mason_tool_installer.setup({
+		ensure_installed = tools,
+	})
 end
 
 return M
