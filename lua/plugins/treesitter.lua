1ef3da231af009a2d1d286cf04b591251e7863e7
am-david-jiang <jiangqiang2009@126.com>
2025-01-01 14:46:19 +0800

    feat: add new languages for tree-sitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,23 +7,35 @@ local M = {
 function M.config()
 	require("nvim-treesitter.configs").setup({
 		ensure_installed = {
-			"lua",
-			"markdown",
-			"markdown_inline",
-			"bash",
-			"python",
-			"javascript",
-			"typescript",
+			-- Compiled languages
 			"c",
 			"cpp",
 			"cmake",
+			"rust",
+			-- Dynamic languages
+			"lua",
+			"python",
+			-- Script languages
+			"bash",
+			"fish",
+			-- Web development
+			"html",
+			"css",
+			"javascript",
+			"typescript",
+			"tsx",
+			-- Markup languages
+			"yaml",
+			"toml",
+			-- Markdown language for documentation
+			"markdown",
+			"markdown_inline",
 		},
 		auto_install = true,
 		highlight = {
 			enable = true,
 			use_languagetree = true,
 		},
-
 		indent = {
 			enable = true,
 		},
