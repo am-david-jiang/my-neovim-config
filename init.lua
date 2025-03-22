262fe2d2058c61a8249be4ab04c13871ceac850a
am-david-jiang <jiangqiang2009@126.com>
2025-03-22 17:14:39 +0800

    refactor: migrate lazy.nvim config file to config/ directory and use `spec` property to import all the specs of plguins in plugins/ directory

--- init.lua
+++ init.lua
@@ -1,58 +1,60 @@
 require("options")
 require("keymap")
 
-local spec = require("launch")
+-- local spec = require("launch")
+--
+-- -- Colorscheme
+-- spec("colorscheme")
+-- -- Devicons
+-- spec("plugins.devicons")
+-- -- Which-Key integration
+-- spec("plugins.whichkey")
+-- -- Nvim Tree
+-- spec("plugins.nvimtree")
+-- -- Programming
+-- -- - Treesitter
+-- spec("plugins.treesitter")
+-- -- - Nvim-cmp configuration
+-- spec("plugins.cmp")
+-- -- - LSP configuration
+-- spec("plugins.mason")
+-- spec("plugins.schemastore")
+-- spec("plugins.lspconfig")
+-- spec("plugins.illuminate")
+-- -- Formatting
+-- spec("plugins.conform")
+-- -- Linting
+-- spec("plugins.nvimlint")
+-- -- Lualine
+-- spec("plugins.lualine")
+-- -- Comments
+-- spec("plugins.comment")
+-- -- Breadcrumbs
+-- spec("plugins.breadcrumbs")
+-- -- Auto Pair
+-- spec("plugins.autopairs")
+-- --   Treesitter Autotag
+-- spec("plugins.ts-autotag")
+-- -- Indent line
+-- spec("plugins.indentline")
+-- -- Telescope
+-- spec("plugins.telescope")
+-- -- Project
+-- spec("plugins.project")
+-- -- Git Signs
+-- spec("plugins.gitsigns")
+-- -- Neogit
+-- spec("plugins.neogit")
+-- -- Trouble.nvim
+-- spec("plugins.trouble")
+-- -- Dashboard Screen
+-- spec("plugins.alpha")
+-- -- Toggle terminal
+-- spec("plugins.toggleterm")
+-- -- Tabby.nvim for tab bar
+-- spec("plugins.tabby")
+--
+-- -- Apply all plugins spec via lazy.nvim
+-- require("lazy_manager")
 
--- Colorscheme
-spec("colorscheme")
--- Devicons
-spec("plugins.devicons")
--- Which-Key integration
-spec("plugins.whichkey")
--- Nvim Tree
-spec("plugins.nvimtree")
--- Programming
--- - Treesitter
-spec("plugins.treesitter")
--- - Nvim-cmp configuration
-spec("plugins.cmp")
--- - LSP configuration
-spec("plugins.mason")
-spec("plugins.schemastore")
-spec("plugins.lspconfig")
-spec("plugins.illuminate")
--- Formatting
-spec("plugins.conform")
--- Linting
-spec("plugins.nvimlint")
--- Lualine
-spec("plugins.lualine")
--- Comments
-spec("plugins.comment")
--- Breadcrumbs
-spec("plugins.breadcrumbs")
--- Auto Pair
-spec("plugins.autopairs")
---   Treesitter Autotag
-spec("plugins.ts-autotag")
--- Indent line
-spec("plugins.indentline")
--- Telescope
-spec("plugins.telescope")
--- Project
-spec("plugins.project")
--- Git Signs
-spec("plugins.gitsigns")
--- Neogit
-spec("plugins.neogit")
--- Trouble.nvim
-spec("plugins.trouble")
--- Dashboard Screen
-spec("plugins.alpha")
--- Toggle terminal
-spec("plugins.toggleterm")
--- Tabby.nvim for tab bar
-spec("plugins.tabby")
-
--- Apply all plugins spec via lazy.nvim
-require("lazy_manager")
+require('config.lazy')
