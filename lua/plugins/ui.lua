5fc0b6d936a6d5b655d03781d787577b6bab30dc
am-david-jiang <jiangqiang2009@126.com>
2025-03-22 20:31:17 +0800

    feat: install noice.nvim plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,6 +1,7 @@
 ---@module 'lazy'
 ---@type LazySpec[]
 local M = {
+  -- Lualine configuration
   {
     "nvim-lualine/lualine.nvim",
     opts = {
@@ -18,6 +19,42 @@ local M = {
         lualine_z = { "location" },
       },
       extensions = { "quickfix", "man", "fugitive" },
+    },
+  },
+  -- Noice.nvim configuration
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+            },
+          },
+          view = "mini",
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+      },
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
     },
   },
 }
