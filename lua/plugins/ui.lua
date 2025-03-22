bfa9c9dc3fd58baa7db20b64449298334418315b
am-david-jiang <jiangqiang2009@126.com>
2025-03-22 20:27:49 +0800

    refactor: move lualine.nvim config to ui.lua file

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,25 @@
+---@module 'lazy'
+---@type LazySpec[]
+local M = {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        ignore_focus = { "NvimTree" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      extensions = { "quickfix", "man", "fugitive" },
+    },
+  },
+}
+
+return M
