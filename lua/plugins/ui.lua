f85b1fc45a625fda2798b8c22b4a467cf0f586f5
am-david-jiang <jiangqiang2009@126.com>
2025-03-29 22:36:06 +0800

    refactor: replace tabby.nvim with barbar.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -57,6 +57,18 @@ local M = {
       "rcarriga/nvim-notify",
     },
   },
+  -- Tabbar configuration
+  {
+    "romgrk/barbar.nvim",
+    dependencies = {
+      "lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+      "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {},
+  },
 }
 
 return M
